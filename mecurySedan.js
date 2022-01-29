bea4a1db66fa93d90d6e843e18a47e9766afae37
prashantkumar1113 <prashantkumar1113@gmail.com>
2022-01-29 09:41:03 -0600

    Added a package.json and start script

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,19 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+//const VehicleModule = require("./vehicle");
+const { Vehicle } = require("./vehicleBaseClass");
+
+class Car extends Vehicle {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.maxPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maxSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+}
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+let v = new Car("Mecury", "Sedan", "1965", "color", "mileage");
+console.log(v.make);
